bde5874c93aa9a9b11011f68f9109439f821f14e
Slavko Zitnik <slavko@zitnik.si>
2012-07-12 15:53:34 +0200

    cetrtek

---
+++ src/main/java/org/openblend/fejstbuk/cdidemo/StringCapitalizer.java
@@ -0,0 +1,11 @@
+package org.openblend.fejstbuk.cdidemo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slavkoz
+ * Date: 7/12/12
+ * Time: 10:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCapitalizer {
+}
---
+++ src/main/java/org/openblend/fejstbuk/ui/ClockRestService.java
@@ -0,0 +1,11 @@
+package org.openblend.fejstbuk.ui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slavkoz
+ * Date: 7/12/12
+ * Time: 3:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClockRestService {
+}
